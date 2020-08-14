a3d3d102c2ca80e012237401173597ed07afaa83
Kevin <kdbautis@espol.edu.ec>
2020-08-13 22:57:41 -0500

    Refactorizando últimos Code Smells

--- src/modelos/Materia.java
+++ src/modelos/Materia.java
@@ -7,5 +7,30 @@ public class Materia {
     public double notaInicial;
     public double notaFinal;
     public double notaTotal;
+
+    public double getNotaInicial() {
+        return notaInicial;
+    }
+    
+    public void setNotaInicial(double notaInicial) {
+        this.notaInicial = notaInicial;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
+    public double getNotaTotal() {
+        return notaTotal;
+    }
+
+    public void setNotaTotal(double notaTotal) {
+        this.notaTotal = notaTotal;
+    }
+    
     
 }
--- src/modelos/Profesor.java
+++ src/modelos/Profesor.java
@@ -9,9 +9,15 @@ public class Profesor {
     public int edad;
     public String direccion;
     public String telefono;
-    public InformacionAdicionalProfesor info;
     public ArrayList<Paralelo> paralelos;
+    public int añosdeTrabajo;
+    public String facultad;
+    public double BonoFijo;
 
+    public double calcularSueldo(Profesor prof){
+        return (prof.getAñosdeTrabajo()*60 + prof.getBonoFijo());
+    }
+    
     public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
         this.codigo = codigo;
         this.nombre = nombre;
@@ -25,6 +31,28 @@ public class Profesor {
     public void anadirParalelos(Paralelo p){
         paralelos.add(p);
     }
-    
-    
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
 }
